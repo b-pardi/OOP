cd0428c3507dcb29939c14e921567bcb82c315c4
brandonator24 <72064522+brandonator24@users.noreply.github.com>
2022-12-10 11:15:18 -0800

    fix q1 lab 8

--- lab-8/question1/fibonacci.cpp
+++ lab-8/question1/fibonacci.cpp
@@ -1,25 +1,34 @@
 #include <iostream>
 using namespace std;
 
-template <typename T>
-long fibonacci(const T &n)
-{
-    static long *fib = new long[n + 1];
-    fib[0] = 0;
-    fib[1] = 1;
+// all other cases where n > 1 iterate adding previous values
+// default T to int unless otherwise specified
+template<int n, typename T = int>
+class fibonacci {
+public:
+    static constexpr T value = fibonacci<n-1,T>::value + fibonacci<n-2,T>::value;
+};
 
-    for (int index = 2; index < n + 1; index++)
-    {
-        fib[index] = fib[index - 1] + fib[index - 2];
-        cout << index << ". " << fib[index] << endl;
-    }
-    return fib[n];
-}
+// base case 1 where n = 0
+template<typename T>
+class fibonacci<0,T> {
+public:
+    static constexpr T value = 0;
+};
+
+// base case 2 where n = 1
+template<typename T>
+class fibonacci<1,T> {
+public:
+    static constexpr T value = 1;
+};
+
 
 int main()
 {
-    int n = 90;
-    cout << fibonacci(n) << endl;
-    
+    const int n1 = 90;
+
+    // 90th fibonacci number is large, so must specify long data type
+    cout <<fibonacci<n1, long>::value << endl;
     return 0;
 }
